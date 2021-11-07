7b0124cba2da9bfa253328c462e89ac49c8bc9f8
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2021-11-07 08:57:10 -0500

    original commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import FaceMesh from './component/face/Facemesh';
+import Hand from './component/hand/Hand';
+import Pose from './component/pose/Pose';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <nav className="navbar">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src="https://www.watadict.com/img/logo.png" alt="" width="163" height="79" className="d-inline-block align-text-top" />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">FaceMesh</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="hand" className="nav-link">Hand</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="pose" className="nav-link">Pose</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">FaceMesh</Link>
+            </li>
+            <li>
+              <Link to="hand">Hand</Link>
+            </li>
+            <li>
+              <Link to="pose">Pose</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<FaceMesh />}></Route>
+          <Route path="/hand" element={<Hand />}></Route>
+          <Route path="/pose" element={<Pose />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/face/Facemesh.js
@@ -0,0 +1,83 @@
+import React, { useRef, useEffect } from "react";
+import "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+
+function FaceMesh() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const loadModel = async () => {
+    const model = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    setInterval(() => {
+      getFeed(model);
+    }, 10);
+  };
+
+  const getFeed = async (model) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const face = await model.estimateFaces({input:video});
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+    }
+  };
+
+  loadModel()
+
+  return(
+    <div>
+      <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+    </div>
+  )
+}
+
+export default FaceMesh
